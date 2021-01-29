5bdd9b8a157ff8cf609b672bd70b3a971a066e06
andyo <javabudd@gmail.com>
2021-01-29 15:53:46 -0700

    add better exception handling

--- shock.py
+++ shock.py
@@ -27,9 +27,42 @@ class Shock(object):
         self.market = Market(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
         self.trade = Trade(self.api_key, self.api_secret, self.api_passphrase, is_sandbox=self.sandbox)
 
-    def connection_error(self):
-        print('connection error, sleeping for a bit...')
+    def error(self, error):
         time.sleep(self.resolution)
+
+    def create_sell_limit_order(self, price):
+        try:
+            order = shock.trade.create_limit_order(shock.symbol, 'sell', self.leverage, self.size, price,
+                                                   timeInForce='IOC')
+            while shock.trade.get_order_details(order['orderId']).get('isActive'):
+                print('waiting for order execution...')
+        except BaseException as e:
+            print('sell order execution failed!')
+
+            self.error(e)
+
+            return False
+
+        print('sell order executed', 'order id =', order['orderId'])
+
+        return True
+
+    def create_buy_limit_order(self, price):
+        try:
+            order = shock.trade.create_limit_order(shock.symbol, 'buy', self.leverage, self.size, price,
+                                                   timeInForce='IOC')
+            while shock.trade.get_order_details(order['orderId']).get('isActive'):
+                print('waiting for order execution...')
+        except BaseException as e:
+            print('buy order execution failed!')
+
+            self.error(e)
+
+            return False
+
+        print('buy order executed', 'order id =', order['orderId'])
+
+        return True
 
 
 if __name__ == "__main__":
@@ -40,12 +73,12 @@ if __name__ == "__main__":
         time_from = time_to - shock.resolution * 60 * 35 * 1000
         try:
             data = shock.market.get_kline_data(shock.symbol, shock.resolution, time_from, time_to)
-        except ConnectionError as e:
-            shock.connection_error()
+        except BaseException as e:
+            print('kline retrieval failed!')
+            shock.error(e)
             continue
 
         now_price = int(data[-1][4])
-        print('closed price =', now_price)
 
         # high_track
         high = []
@@ -53,7 +86,6 @@ if __name__ == "__main__":
             high.append(data[index][2])
         high.sort(reverse=True)
         high_track = float(high[0])
-        print('high_track =', high_track)
 
         # low_track
         low = []
@@ -61,54 +93,28 @@ if __name__ == "__main__":
             low.append(data[index][3])
         low.sort()
         low_track = float(low[0])
-        print('low_track =', low_track)
 
         # interval_range
         interval_range = (high_track - low_track) / (high_track + low_track)
-        print('interval_range =', interval_range)
 
-        order_flag = 0
         # current position qty of the symbol
         position_details = shock.trade.get_position_details(shock.symbol)
         position_qty = int(position_details['currentQty'])
-        print('current position qty of the symbol =', position_qty)
+
+        order_flag = 0
         if position_qty > 0:
             order_flag = 1
         elif position_qty < 0:
             order_flag = -1
             position_qty = abs(position_qty)
 
-        if order_flag == 1 and now_price > high_track - .5:
-            order = shock.trade.create_limit_order(shock.symbol, 'sell', position_details['realLeverage'],
-                                                   position_qty, now_price, cancel_after='5')
-
-            while shock.trade.get_order_details(order['orderId']).get('isActive'):
-                print('waiting for order execution...')
-            print('order_flag == 1,order id =', order['orderId'])
+        if order_flag == 1 and now_price > high_track and shock.create_sell_limit_order(now_price):
             order_flag = 0
-        elif order_flag == -1 and now_price < low_track + .5:
-            order = shock.trade.create_limit_order(shock.symbol, 'buy', position_details['realLeverage'],
-                                                   position_qty, now_price, cancel_after='5')
-
-            while shock.trade.get_order_details(order['orderId']).get('isActive'):
-                print('waiting for order execution...')
-            print('order_flag == -1,order id =', order['orderId'])
+        elif order_flag == -1 and now_price < low_track and shock.create_buy_limit_order(now_price):
             order_flag = 0
 
         if interval_range < shock.valve and order_flag == 0:
             if now_price > high_track:
-                order = shock.trade.create_limit_order(shock.symbol, 'sell', shock.leverage,
-                                                       shock.size, now_price, cancel_after='5')
-
-                while shock.trade.get_order_details(order['orderId']).get('isActive'):
-                    print('waiting for order execution...')
-                print('now price < high track,sell order id =', order['orderId'])
-                order_flag = -1
+                shock.create_sell_limit_order(now_price)
             elif now_price < low_track:
-                order = shock.trade.create_limit_order(shock.symbol, 'buy', shock.leverage,
-                                                       shock.size, now_price, cancel_after='5')
-
-                while shock.trade.get_order_details(order['orderId']).get('isActive'):
-                    print('waiting for order execution...')
-                print('now price > high track,buy order id =', order['orderId'])
-                order_flag = 1
+                shock.create_buy_limit_order(now_price)
